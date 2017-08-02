df92bb608052d3507db28a4e5aa2d6d589e61278
Taekwon Lee <taekwon@devsisters.com>
2017-08-02 12:51:51 +0900

    Update real-time.py
    Update real-time.py using redis cache, ml request

--- code/real-time.py
+++ code/real-time.py
@@ -1,42 +1,74 @@
-import urllib2
-# If you are using Python 3+, import urllib instead of urllib2
+import os
+import json
+import urllib.request
+import redis
 
-import json 
+# set configs
+redis_host = '<redis host>'
+auth_key = '<redis auth key>'
+redis_db = redis.StrictRedis(host=redis_host, port=6379, db=0, password=auth_key)
 
-data =  {
+url = '<azure ml endpoint>'
+api_key = '<azure ml api key>'
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+redis_expire = 3600
 
+postreqdata = json.loads(open(os.environ['req']).read())
+inputs = postreqdata['inputs']
+
+# make template of data to accept on azure ML
+data_template =  {
         "Inputs": {
-
                 "input1":
                 {
                     "ColumnNames": ["idx", "age", "promotion_num", "identity", "game_play_min_per_day", "item_purchase_num_in_90_days", "game_level", "crystal", "race", "gender", "register_code", "purchase_num", "game_play_num_per_week", "country", "churn_YN"],
-                    "Values": [ [ "0", "0", "0", "0", "0", "0", "0", "0", "value", "value", "0", "0", "0", "value", "value" ], [ "0", "30", "100", "300", "100", "00", "500", "300", "value", "value", "0", "0", "0", "value", "value" ], ]
-                },        },
-            "GlobalParameters": {
-}
+                    "Values": [  ]
+                },        
+            },
+        "GlobalParameters": {}
     }
 
-body = str.encode(json.dumps(data))
+# function for sending data to ML
+def do_ml_request(data):
+    body = str.encode(json.dumps(data_template))
+    req = urllib.request.Request(url, body, headers) 
 
-url = 'https://asiasoutheast.services.azureml.net/workspaces/46d0e60b05b34558827abd41f11d204f/services/7e2a0a94ed4d40c1965b523707feea59/execute?api-version=2.0&details=true'
-api_key = '<API_KEY>' # Replace this with the API key for the web service
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        return result 
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        print(error.reason)
 
-req = urllib2.Request(url, body, headers) 
 
-try:
-    response = urllib2.urlopen(req)
+result_set = dict()
+target_instance_list = list()
 
-    # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
-    # req = urllib.request.Request(url, body, headers) 
-    # response = urllib.request.urlopen(req)
+# filter user index list we should do request
+for instance in inputs:
+    user_idx = instance[0]
+    print('Trying to hit redis User #' + user_idx)
+    result_cached = redis_db.get(user_idx)
 
-    result = response.read()
-    print(result) 
-except urllib2.HTTPError, error:
-    print("The request failed with status code: " + str(error.code))
+    if result_cached is None:
+        target_instance_list.append(instance)
+    else:
+        print('Using Cached Data!')
+        result_set[user_idx] = result_cached.decode("utf-8")
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
+# If we should do request,
+if len(target_instance_list) > 0:
+    data_template["Inputs"]["input1"]["Values"] = target_instance_list
+    result = do_ml_request(data_template).decode("utf-8") 
+    result = json.loads(result)['Results']['output1']['value']['Values']
+    for key, val in enumerate(result):
+        user_idx = target_instance_list[key][0]
+        result_set[user_idx] = val
+        redis_db.set(user_idx, val, ex=redis_expire)
 
-    print(json.loads(error.read()))       
+# fill response
+print(result_set)
+response = open(os.environ['res'], 'w')
+response.write(json.dumps(result_set))
+response.close()
